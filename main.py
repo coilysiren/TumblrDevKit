98a1e84bf42e0d358e9cd057dd86500bf960688a
Lynn Cyrin <firemagelynn@gmail.com>
2015-06-11 23:38:49 -0700

    stuff

--- main.py
+++ main.py
@@ -11,14 +11,39 @@ DEBUG = True
 app.config.from_object(__name__)
 
 def sub_var_tumblr_to_jinja(match):
-    match = match.group(0)
-    # python variables can't have - in the name
+    match = match.group(0).lower()
     match = re.sub(r'\-', '_', match)
-    # tumblr variable -> jinja variable
     match = '{'+match+'}'
     return match
 
+def sub_block_start(match):
+    match = match.group(0).lower()
+    var = match[7:-1]
+    match = '{% if '+var+' %}'
+    return match
+
+def sub_post_variables_and_blocks(match):
+    match = match.group(0).lower()
+    match = 'post.'+match
+    return match
+
+def sub_post_types(match):
+    match = match.group(0).lower()
+    match = 'type==\"'+match+'\"'
+    return match
+
 def tumblify(path):
+    RE_VARIABLE      = r'(?is)\{[A-Z0-9 _\.\-="]*?\}'
+    RE_BLOCK_START   = r'(?is)\{block\:[A-Z0-9 _\.\-="]*?\}'
+    RE_BLOCK_END     = r'(?is)\{\/block\:[A-Z0-9 _\.\-="]*?\}'
+    RE_POSTS         = r'(?is)(?<=\{block\:posts\}).*(?=\{\/block\:posts\})'
+    RE_POSTS_START   = r'(?is)\{block\:posts\}'
+    RE_POSTS_END     = r'(?is)\{\/block\:posts\}'
+    RE_POST_VARIABLE = r'(?is)(?<=\{)[A-Z0-9 _\.\-="]*?(?=\})'
+    RE_POST_BLOCK    = r'(?is)(?<=\{block\:)[A-Z0-9 _\.\-="]*?(?=\})'
+    RE_POST_TYPES    = r'(?is)(?<=\{block\:post\.)(text|photo|panorama|photoset|quote|link|chat|audio|video|answer)(?=\})'
+
+
     # format the html file
     with open('templates/'+path, 'r') as f:
         html = f.readlines()
@@ -27,38 +52,36 @@ def tumblify(path):
         _html += line.strip()
     html = _html
 
+
     # template variable context
-    context = {
-        'output': '\n\n'
-    }
-
+    context = {'output': '\n\n'}
+    # example context
     with open('response.txt', 'r') as f:
-        response = (json.load(f.read()))
-    context['posts'] = response['posts']
+        response = json.load(f)
     context.update(response['response']['blog'])
+    context['posts'] = response['response']['posts']
+    context['metadescription'] = context['description']
 
-    print(context)
+    posts_index = re.search(RE_POSTS, html)
+    posts = html[posts_index.start():posts_index.end()]
+    posts = re.sub(RE_POST_VARIABLE, sub_post_variables_and_blocks, posts)
+    posts = re.sub(RE_POST_BLOCK, sub_post_variables_and_blocks, posts)
+    posts = re.sub(RE_POST_TYPES, sub_post_types, posts)
+    html = html[:posts_index.start()] + posts + html[posts_index.end():]
 
-    RE_VARIABLE = r'(?is)(?<!\{)\{[A-Z0-9 _-]*\}(?!\})'
-    RE_BLOCK    = r'(?is)\{block\:[A-Z0-9 _-]*\}.*\{\/block\:[A-Z0-9 _-]*\}'
-    RE_POSTS    = r'(?is)\{block\:posts\}.*\{\/block\:posts\}'
 
-    post_types = ['text', 'photo', 'panorama', 'photoset', 'quote', 'link', 'chat', 'audio', 'video', 'answer']
-
-    # parse all variables
     html = re.sub(RE_VARIABLE, sub_var_tumblr_to_jinja, html)
+    html = re.sub(RE_POSTS_START, '{% for post in posts %}', html)
+    html = re.sub(RE_POSTS_END, '{% endfor %}', html)
+    html = re.sub(RE_BLOCK_START, sub_block_start, html)
+    html = re.sub(RE_BLOCK_END, '{% endif %}', html)
 
-    # parse blocks
-    # parse post variables
-    # parse post types
-    # parse post blocks
 
-    # formatting
     context['output'] += '\n\n'
     from bs4 import BeautifulSoup
     html = BeautifulSoup(html).prettify()
 
-    print(html)
+
     return html, context
 
 @app.route('/')
