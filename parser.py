aaebd90e02ee9f531d6b2a3e2fada6266f90c788
Lynn Cyrin <firemagelynn@gmail.com>
2015-10-24 00:44:20 +0000

    update parser

--- parser.py
+++ parser.py
@@ -36,18 +36,27 @@ def parse_theme(path):
     RE_POST_BLOCK    = r'(?is)(?<=\{block\:)[A-Z0-9 _\.\-="]*?(?=\})'
     RE_POST_TYPES    = r'(?is)(?<=\{block\:post\.)(text|photo|panorama|photoset|quote|link|chat|audio|video|answer)(?=\})'
 
+    
+    # pull out posts block
     posts_index = re.search(RE_POSTS, html)
     posts = html[posts_index.start():posts_index.end()]
-    posts = re.sub(RE_POST_VARIABLE, _sub_post_variables_and_blocks, posts)
-    posts = re.sub(RE_POST_BLOCK, _sub_post_variables_and_blocks, posts)
+    # reformat (TYPE) to (posts.type == TYPE)
     posts = re.sub(RE_POST_TYPES, _sub_post_types, posts)
+    # reformat (VAR) to (posts.VAR)
+    posts = re.sub(RE_POST_VARIABLE, _sub_post_variables_and_blocks, posts)
+    # reformat (BOOL) to (posts.BOOL)
+    posts = re.sub(RE_POST_BLOCK, _sub_post_variables_and_blocks, posts)
+
+    # return posts to rest of HTML
     html = html[:posts_index.start()] + posts + html[posts_index.end():]
-
-
+    # change tumblr variables to jinja variables
     html = re.sub(RE_VARIABLE, _sub_var_tumblr_to_jinja, html)
-    html = re.sub(RE_POSTS_START, '{% for post in posts %}', html)
-    html = re.sub(RE_POSTS_END, '{% endfor %}', html)
+    # swap every block except posts to an if block
     html = re.sub(RE_BLOCK_START, _sub_block_start, html)
     html = re.sub(RE_BLOCK_END, _sub_block_end, html)
+    # swap the posts block to a for block
+    html = re.sub(RE_POSTS_START, '{% for post in posts %}', html)
+    html = re.sub(RE_POSTS_END, '{% endfor %}', html)
+
 
     return html
