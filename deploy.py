acc2bbc6a8223bcc810d142701fd476e0628d5e9
Lynn Cyrin <firemagelynn@gmail.com>
2015-11-02 19:21:53 -0800

    deploy script

---
+++ deploy.py
@@ -0,0 +1,27 @@
+from env import ENV
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+select = driver.find_element_by_css_selector
+
+driver.get('http://localhost:5000/static/index.html')
+select('*').send_keys(Keys.CONTROL, 'a')
+select('*').send_keys(Keys.CONTROL, 'c')
+
+driver.get('https://www.tumblr.com/customize')
+if 'Log in' in driver.title:
+    sleep(0.1)
+    select('#signup_email').send_keys(ENV['EMAIL'])
+    select('#signup_password').send_keys(ENV['PASS'])
+    select('#signup_forms_submit').click()
+sleep(4)
+select('#edit_html_button').click()
+sleep(0.1)
+select('html').send_keys(Keys.TAB, Keys.CONTROL, 'a')
+select('html').send_keys(Keys.TAB, Keys.DELETE)
+select('html').send_keys(Keys.TAB, Keys.CONTROL, 'v')
+select('#edit_html_panel .buttons_right .button.green').click() # update
+select('#edit_html_panel .buttons_right .button.blue').click() # save
+select('#edit_html_panel .buttons_left .icon_arrow_thin_left').click() # back
