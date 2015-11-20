176363103cadb78963d40b4ffb5fcc695927e266
Lynn Cyrin <firemagelynn@gmail.com>
2015-11-20 12:00:19 -0800

    update main with 'build_themes'
    also remove unused code, its really unlikely to return anytime soon

--- main.py
+++ main.py
@@ -7,39 +7,14 @@ from os import environ as ENV
 import flask
 from dotenv import load_dotenv
 
-
-def html_escape(func):
-    def wrapped(*args, **kwargs):
-        from html import escape
-        return escape(func(*args, **kwargs))
-    return wrapped
+# local
+import deploy
 
 
 load_dotenv('.env')
 app = flask.Flask(__name__)
-
-
-# @app.context_processor
-# def add_data_to_context():
-#     with open('data.json', 'r') as f:
-#         data = json.load(f)
-
-#     data.update(data['response']['blog'])
-#     data['posts'] = data['response']['posts']
-#     data['metadescription'] = data['description']
-
-#     return data
-
-@app.route('/static/themes/<path>')
-@html_escape
-def theme_server(path):
-    with open('static/themes/'+path, 'r') as f:
-        string = f.read()
-    return string
-
-# @app.route('/')
-# def index ():
-#     return flask.render_template('index.html')
+@app.before_first_request(deploy.build_themes)
+# find themes at static/themes/build/blogname.html
 
 
 if __name__ == '__main__':
