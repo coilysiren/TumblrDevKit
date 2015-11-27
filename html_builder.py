4227ef4287b08af0634da0fe00fc722a5c697cf6
Lynn Cyrin <firemagelynn@gmail.com>
2015-11-26 22:23:11 -0800

    add metadata parsing to html builder

--- html_builder.py
+++ html_builder.py
@@ -2,44 +2,74 @@
 import re
 from glob import glob
 from html import escape
+from pprint import pprint
+from os import environ as ENV
+from difflib import context_diff
+
+# external
+from dotenv import load_dotenv
 
 
+load_dotenv('.env')
 style_tag = '<style type="text/css" source="local">{}</style>'
-# expands to (ex:) <meta name="color:Background" content="#eee"/>
 metadata_tag = '<meta name="{}" content="{}"/>'
 
 
+def make_diff(original, edited):
+    if ENV['DEBUG']:
+        diff = context_diff(original.splitlines(), edited.splitlines())
+        print('\nhtml diff: \n\n')
+        pprint(list(diff))
+        print('\n')
+
 def format_metadata(blog_name, html):
+    _html = html
 
     if metadata_tag not in html:
         print('[WARNING] Could not find '+metadata_tag+' for theme '+blog_name)
         return html
 
     else:
+        metadata_replacement = ''
+
+        # get sass file
         sass_file = glob('static/sass/'+blog_name+'.*')[0]
         with open(sass_file, 'r') as f:
             sass = f.read()
 
-        # for example content:
-        # $color_primary: unquote("{color:primary}")
-        # $color_primary: rgb(244, 231, 144) !default
-        variables = re.search('''
-            ^(\$\w+):                       # $color_primary:
-                \s*unquote\                 # unqoute
-                (\"\{([\w\s:]+)\}\"\);*     # ("{color:primary")
-                (?:\s\#.*?)*?\s*?           # any comments / whitespace
+        # get the variables to input into the html
+        # inside the sass file they should look like so:
+        #
+        # $color_VAR: unquote("{color:VAR}")
+        # $color_VAR: rgb(XXX, XXX, XXX) !default
+        sass_variables = re.findall(\
+            r'(?im)'+\
+            r'^(\$\w+):'+\
+                r'\s*unquote'+\
+                r'\(\"\{([\w\s:]+)\}\"\);*'+\
+                r'(?:\s\#\s\S*?)*?'+\
+            r'\s*?\1:'+\
+                r'\s([\w\s,\"\\#\-\(\)]*?)'+\
+                r'\s*?!default;*$'\
+            ,sass)
 
-            \1:                             # $color_primary:
-                \s([\w\s,\-\(\)]*?)         # rgb(244, 231, 144)
-                !default;*$                 # !default
-            ''', sass, flags=[re.I, re.M, re.X])
+        if not sass_variables:
+            print('[WARNING] Variables in '+sass_file+' not represent or incorrectly formatted')
+            return html
 
-        print(variables)
+        # format the variables into metadata tags
+        for variable in sass_variables:
+            _replace = metadata_tag.format(variable.group(2), variable.group(3))
+            metadata_replacement += _replace+'\n'
 
+        # add tags to the html
+        html.replace(metadata_tag, metadata_replacement)
+        make_diff(_html, html)
         return html
 
 
 def format_style(blog_name, html):
+    _html = html
     style_file = glob('static/css/'+blog_name'.*')
 
     # check if a style file with the same name as the blog exists
@@ -57,8 +87,9 @@ def format_style(blog_name, html):
         css = f.read()
     css += '{CustomCSS}'
     style_replacement = style_tag.format(css)
-    html = html.replace(style_tag, style_replacement)
 
+    html = html.replace(style_tag, style_replacement)
+    make_diff(_html, html)
     return html
 
 
